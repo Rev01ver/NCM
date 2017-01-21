875ebee1c69802e5304cbb7266a1b3acd66f4178
Ilya Kirienkov <ycant@bk.ru>
2017-01-21 13:38:46 +0300

    Добавлена функция проверки установленного времени на циске

--- app/utils/utils.py
+++ app/utils/utils.py
@@ -1,6 +1,7 @@
 import paramiko
 import re, datetime
 
+
 def get_last_rev():
     return None
 
@@ -11,6 +12,19 @@ def get_rev_by_id():
 
 def save_rev():
     return None
+
+
+#проверка даты на cisco
+def check_date(hostname,username,password):
+    cisco_date = get_conf_date(hostname, username, password, 3)
+    today_time = datetime.datetime.strftime(datetime.datetime.today(),"%Y-%m-%d %H:%M")
+    cisco_date = datetime.datetime.strftime(cisco_date,"%Y-%m-%d %H:%M")
+    if cisco_date != today_time:
+        bad_date = 1
+    else:
+        bad_date = 0
+    return bad_date
+
 
 #Уведомление о том, что изменения в running-config не сохранены в startup-config
 def do_wr(hostname,username,password):
@@ -47,9 +61,11 @@ def get_conf(hostname,username,password,type):
     #1,10 - running-config
     #2,20 - startup-config
     if type == 1:
-        stdin, stdout, stderr = ssh.exec_command('sh  run view full')
+        stdin, stdout, stderr = ssh.exec_command('sh run view full')
     elif type ==2:
-        stdin, stdout, stderr = ssh.exec_command('sh  start')
+        stdin, stdout, stderr = ssh.exec_command('sh start')
+    elif type ==3:
+        stdin, stdout, stderr = ssh.exec_command('sh clock')
     elif type == 10:
         stdin, stdout, stderr = ssh.exec_command('sh start | include Last configuration')
     elif type == 20:
